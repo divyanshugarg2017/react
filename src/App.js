f51dda73f978eb4aa3e3f8057da31e51230de991
Divyanshu Garg <divyanshugarg1995@gmail.com>
2020-04-15 14:36:26 -0500

    wrong code appjs error

--- src/App.js
+++ src/App.js
@@ -3,21 +3,35 @@ import logo from './logo.svg';
 import {Navbar,NavbarBrand} from 'reactstrap';
 import Menu from './components/MenuComponents';
 import './App.css';
+import {DISHES} from './shared/dishes';
+import { render } from '@testing-library/react';
 
 function App() {
-  return (
-    <div>
-      <Navbar dark color="primary">
-        <div className="container">
-          <NavbarBrand href="/">ristorante con fusion</NavbarBrand>
-        </div>
 
-      </Navbar>
-      <Menu />
-      
-     
-    </div>
-  );
-}
+  constructor (props) {
+    super(props);
+    this.state= {
+      dishes: DISHES
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">ristorante con fusion</NavbarBrand>
+          </div>
+  
+        </Navbar>
+  
+        <Menu dishes={this.state.dishes} />
+        
+       
+      </div>
+    );
+  }
+  }
+
 
 export default App;
